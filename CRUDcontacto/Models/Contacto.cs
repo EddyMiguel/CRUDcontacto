b20b318491f9d6352c8f2a198cc2041e61ff11af
Eddy Martinez <eddymiguelmartinez2004@gmail.com>
2026-05-01 21:29:00 -0400

    Agregar archivos de proyecto.

---
+++ CRUDcontacto/Models/Contacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDcontacto.Models;
+
+public partial class Contacto
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Telefono { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string? Direccion { get; set; }
+}
---
+++ CRUDcontacto/Models/ContactosDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDcontacto.Models;
+
+public partial class ContactosDbContext : DbContext
+{
+    public ContactosDbContext()
+    {
+    }
+
+    public ContactosDbContext(DbContextOptions<ContactosDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=ContactosDb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__contacto__3214EC0771135C45");
+
+            entity.ToTable("contacto");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .HasColumnName("correo");
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(100)
+                .HasColumnName("direccion");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(25)
+                .HasColumnName("telefono");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
